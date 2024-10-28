45c8ec748350822c32724b3bcf54fcdd3d42cd0b
DavidAlejandroHA <99730787+DavidAlejandroHA@users.noreply.github.com>
2024-10-28 09:05:16 +0000

    Import LeanTween + GameUIManager vacío
    Import de las librerías del leantween + preparatorio del script del GameUIManager

---
+++ Assets/Scripts/UI/GameUIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUIManager : MonoBehaviour
+{
+    public static GameUIManager Instance { get; private set; }
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
