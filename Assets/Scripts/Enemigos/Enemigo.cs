e7ecc2243c4689bfe02fcd6f215efe6942ec619d
DavidAlejandroHA <99730787+DavidAlejandroHA@users.noreply.github.com>
2024-11-05 13:30:41 +0000

    Implementando las IAs de los enemigos

---
+++ Assets/Scripts/Enemigos/Enemigo.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Enemigo : MonoBehaviour
+{
+
+    protected NavMeshAgent agent;
+    //Ataque
+    [Header("Ataque")]
+    public float attackDamage;
+
+    [Header("Vida")]
+    public float health;
+
+    // Variables
+    [Header("Variables Enemigo IA")]
+    public float speed;
+    public float actionRadio;
+    public bool showActionRadio;
+    public float cooldown;
+
+    bool canAttack;
+    int enemyMask = 1 << 6;
+
+    public abstract void WhileWalking();
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    protected Transform NearestForestHearthPos(Collider[] collisionsList)
+    {
+        Transform nearestForestHearthPos = null;
+        float minorDistance = Mathf.Infinity;
+
+        // Se comprueba y elige al corazón con menor distancia
+        if (collisionsList.Length > 0)
+        {
+            foreach (Collider col in collisionsList)
+            {
+                // Distancia entre el enemigo y el objetivo
+                float actualDistance = Vector3.Distance(transform.position, col.transform.position);
+                if (actualDistance < minorDistance)
+                {
+                    /* Se detectan enemigos dentro del radio de acción pero hay que comprobar que
+                     * no hay muros por delante*/
+                    if (ThereAreNoObstacles(col.transform))
+                    {
+                        minorDistance = actualDistance;
+                        nearestForestHearthPos = col.transform;
+                    }
+                }
+            }
+        }
+        return nearestForestHearthPos; // puede llegar a ser nulo si no hay nada al rededor, hay que                    
+    }                               // tenerlo en cuenta
+
+    protected bool ThereAreNoObstacles(Transform closestObjetive)
+    {
+        bool enemyIsVisible = true;
+        if (closestObjetive != null)
+        {
+            /* Primero intenté esta parte con raycastall pero al final solo funciono con un linecast, 
+             * lo dejo como nota por si acaso*/
+
+            RaycastHit hit;
+            if (Physics.Linecast(transform.position, closestObjetive.position, out hit))
+            {
+                if (hit.transform.tag != "Proyectil" && hit.collider.gameObject.tag != "Enemigo"
+                    && hit.transform.tag != "Gnomo")
+                {
+                    enemyIsVisible = false;
+                }
+            }
+        }
+
+        return enemyIsVisible;
+    }
+
+}
