f73537c73076185f7794b6a86bef37c6a27a3408
leito <carlotacarballolol@gmail.com>
2024-11-20 13:05:35 +0000

    Mejora de script y navigation
    Crear escena de prueba para el navigation y poner objetos de prueba. Instalar también el AI de camino

--- Assets/Scripts/Seta/Mov_Seta.cs
+++ Assets/Scripts/Seta/Mov_Seta.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mov_Seta : MonoBehaviour
 {
@@ -8,29 +10,45 @@ public class Mov_Seta : MonoBehaviour
     int radio = 5;
     float vida = 10;
     float fuerza = 20;
+    public Transform enemigoMaza; //maza o el que sea que sigue la seta
 
     //CONTROLADORES
     Animator animator;
     Rigidbody rb;
-
+    NavMeshAgent NavAgent;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+        NavAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SeguirEnemigo();
+    }
+    void SeguirEnemigo()
+    {
+        Collider[] listaChoques;
+        listaChoques = Physics.OverlapSphere(transform.position, radio);
+        foreach (Collider enemigo in listaChoques)
+        {
+            Debug.Log(enemigo);
+            if (enemigo.CompareTag("Enemy"))
+            {
+                NavAgent.SetDestination(enemigoMaza.position);
+            }
+        }
+
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject)
         {
-            
+            // cuando colisiones hacer animacion de atacar 
         }
     }
     private void OnDrawGizmos()
