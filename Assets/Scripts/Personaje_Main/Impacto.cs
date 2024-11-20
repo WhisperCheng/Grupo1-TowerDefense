f0dd175811db7b9c3ff0b624edbb6f17c7c58f69
Omarchg <omarchaglez@gmail.com>
2024-11-20 09:10:35 +0000

    ParticulaImpacto

--- Assets/Scripts/Personaje_Main/Impacto.cs
+++ Assets/Scripts/Personaje_Main/Impacto.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Impacto : MonoBehaviour
 {
     // Update is called once per frame
+    [SerializeField] GameObject particulasImpacto;
     void OnCollisionEnter(Collision collision)
     {
+        GameObject impacto = Instantiate(particulasImpacto);
+        impacto.transform.position = transform.position;
         Destroy(gameObject); 
     }
 }
