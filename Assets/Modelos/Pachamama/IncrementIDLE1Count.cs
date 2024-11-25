3e795d8d2a14f3229192754f7353753d17a0c623
PotatoeGoat <147033284+PotatoeGoat@users.noreply.github.com>
2024-11-25 08:24:45 +0000

    Update IncrementIDLE1.cs

--- Assets/Modelos/Pachamama/IncrementIDLE1Count.cs
+++ Assets/Modelos/Pachamama/IncrementIDLE1Count.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IncrementAnimacion1Count : StateMachineBehaviour
+public class IncrementIDLE1Count : StateMachineBehaviour
 {
    
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
