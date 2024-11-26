75a5762457b1a977e66ddb7128a1005feecd8fa0
Dxvah <idayraperezdelgado@gmail.com>
2024-11-26 09:14:29 +0000

    LeanTweenIntro modificado
    - El código está completo y funcional

--- Assets/Scripts/UI/LeanTweenIntro.cs
+++ Assets/Scripts/UI/LeanTweenIntro.cs
@@ -8,15 +8,20 @@ public class LeanTweenIntro : MonoBehaviour
 
     [SerializeField]
     GameObject logoImage;
-    // Start is called before the first frame update
+    [SerializeField]
+    GameObject introCanvas;
+
     void Start()
     {
-        
-    }
+        LeanTween.moveY(logoImage.GetComponent<RectTransform>(), 0, 2.7f).setDelay(0.5f).setEase(LeanTweenType.easeInOutBack);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
     }
+  private void BajarAlpha()
+    {
+
+        LeanTween.alpha(introCanvas.GetComponent<RectTransform>(), 0f, 1f).setDelay(1f);
+        introCanvas.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+}
 }
