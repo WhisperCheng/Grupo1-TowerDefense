2f93ee07d9142c259c508f021b18043e70b72db0
AdcoCN <idayraperezdelgado@gmail.com>
2024-12-12 09:33:59 +0000

    Botones cambiar idioma funcionalidad
    - Se ha implementado una mejora en el código de cambiar de idioma. Ahora si pulsas varias veces uno u otro botón se cambia al siguiente idioma, dando igual el puesto en la lista en el que esté

--- Assets/Scripts/UI/Botones/ChangeLanguage.cs
+++ Assets/Scripts/UI/Botones/ChangeLanguage.cs
@@ -7,13 +7,23 @@ using UnityEngine.Localization.Settings;
 
 public class CambiarIdioma : MonoBehaviour
 {
-
-
-
-    public void ActualizarIdioma(int LocalizationTable)
+    int idiomaActual = 0;
+    List<Locale> listaIdiomas;
+    void Start()
     {
-
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[LocalizationTable];
+        listaIdiomas = LocalizationSettings.AvailableLocales.Locales;
     }
+
+    public void CambiarIdiomaArriba()
+    {
+        idiomaActual = (idiomaActual - 1 + listaIdiomas.Count) % listaIdiomas.Count;
+        LocalizationSettings.SelectedLocale = listaIdiomas[idiomaActual];
+    }
+    public void CambiarIdiomaAbajo()
+    {
+        idiomaActual = (idiomaActual + 1) % listaIdiomas.Count;
+        LocalizationSettings.SelectedLocale = listaIdiomas[idiomaActual++];
+    }
+
 }
 
