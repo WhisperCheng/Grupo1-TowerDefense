bb06772d2d67962b8258632154654ed97ba4daee
DavidAlejandroHA <99730787+DavidAlejandroHA@users.noreply.github.com>
2025-01-15 13:54:51 +0000

    Integrando prototipo Hold para vender torres
    Al presionar el botón de vender torres, se mostrará una rueda que se irá cargando a medida que se presiona hasta llegar a cierto punto.

--- Assets/Scripts/Juego/TowerInteractionManager.cs
+++ Assets/Scripts/Juego/TowerInteractionManager.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class TowerInteractionManager : MonoBehaviour
 {
     [Header("Porcentaje de gemas recibidas al vender torre")]
     [Range(0, 100)]
     public float sellingPercentageAmount;
+
+    [Header("Tiempo de esperas")]
+    public float sellingTowerTime;
+    public float upgradingTowerTime;
+
+    public Image spriteTowerInteractions;
+
+    private bool sellingButtonPressed;
 
     public static TowerInteractionManager Instance { get; private set; }
     private void Awake()
@@ -24,7 +33,7 @@ public class TowerInteractionManager : MonoBehaviour
 
     public void SellTower(InputAction.CallbackContext ctx)
     {
-        if (ctx.performed)
+        /*if (ctx.performed)
         {
             Ray rayo = Camera.main.ScreenPointToRay(GameUIManager.Instance.crossHead.transform.position);
             if (Physics.Raycast(rayo, out RaycastHit golpeRayo))
@@ -46,8 +55,38 @@ public class TowerInteractionManager : MonoBehaviour
                     torre.ReturnToPool();
                 }
             }
+        }*/
+
+        // To keep the state in a boolean.
+        sellingButtonPressed = (!ctx.started || ctx.performed) ^ ctx.canceled;
+
+        // When the key is pressed down.
+        if (ctx.started && !ctx.performed)
+        {
+            spriteTowerInteractions.enabled = true;
+            Debug.Log("A");
         }
 
-        //if (context.canceled && !context.performed) // Si se levanta la tecla antes de tiempo
+        // When the key is lifted up.
+        if (ctx.canceled && !ctx.performed) // Si se levanta la tecla antes de tiempo
+        {
+            spriteTowerInteractions.fillAmount = 0;
+            Debug.Log("B");
+            spriteTowerInteractions.enabled = false;
+        }
+        // https://stackoverflow.com/questions/75216584/detect-when-key-lifted-new-unity-input-system
     }
+
+    private void Update()
+    {
+        if (sellingButtonPressed)
+        {
+            spriteTowerInteractions.fillAmount += Time.deltaTime;
+        }
+        /*else
+        {
+            spriteTowerInteractions.fillAmount = 0;
+        }*/
+    }
+
 }
