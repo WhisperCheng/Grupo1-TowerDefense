9aea0b86005850904d9de30b6c715406ab61222f
SrCheese44 <sr.cheese44@gmail.com>
2025-01-15 21:12:38 +0000

    Implementación música en "sergiotest"
    implementación muy básica de la música en mi escena, aún falta meterle el fragor y todos los SFX

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using System.Dynamic;
+using FMOD.Studio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    private EventInstance musicEventInstance;
+
+    public static AudioManager instance { get; private set; }
+
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Audio Manager in the scene.");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        InitializeMusic(FMODEvents.instance.music);
+    }
+
+    public EventInstance CreateInstance (EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        return eventInstance;
+    }
+    private void InitializeMusic(EventReference musicEventReference)
+    {
+        musicEventInstance = CreateInstance(musicEventReference);
+        musicEventInstance.start();
+    }
+
+}
