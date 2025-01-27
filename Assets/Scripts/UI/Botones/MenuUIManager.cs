c4430ae3e4b2ecce9ed8774f877ec1887a12aaa8
Dxvah <idayraperezdelgado@gmail.com>
2025-01-27 19:29:44 +0000

    Añadido booleano de animación a MenuUIManager
    Este booleano comprueba si se ha el botón tiene o no animación al inicio. Con eso podremos animar los botones en función de lo que necesitemos!

--- Assets/Scripts/UI/Botones/MenuUIManager.cs
+++ Assets/Scripts/UI/Botones/MenuUIManager.cs
@@ -8,9 +8,18 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
-public class MenuUIManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class MenuUIManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public static MenuUIManager Instance { get; private set; }
+
+    [Header("Animation Parameters")]
+    [SerializeField] private LeanTweenType animationButton;
+    [SerializeField] private Vector2 finalPosition;
+    [SerializeField] private Vector3 originalScale;
+    [SerializeField] private bool hasInitialAnimation = false; 
+    [SerializeField] private float animationDuration = 0.5f; 
+
+    private bool animationCompleted = false; 
 
     private void Awake()
     {
@@ -24,31 +33,52 @@ public class MenuUIManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         }
     }
 
-    [Header("Animation Parameters")]
-    [SerializeField] private LeanTweenType animationButton;
-    Vector2 finalPosition;
-    [SerializeField] Vector3 originalScale;
-
     private void Start()
     {
         originalScale = transform.localScale;
 
+        
+        if (hasInitialAnimation)
+        {
+            ExecuteInitialAnimation();
+        }
+    }
+
+    private void ExecuteInitialAnimation()
+    {
+        LeanTween.move(gameObject, finalPosition, animationDuration).setEase(animationButton).setOnComplete(() =>
+        {
+            animationCompleted = true; 
+        });
     }
 
     internal void ExecuteAnimation(GameObject gameObject, float delay, Vector2 finalPos)
     {
-        LeanTween.move(gameObject, finalPos, delay -0.5f).setEase(animationButton);
-
+        LeanTween.move(gameObject, finalPos, delay).setEase(animationButton);
     }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         LeanTween.scale(gameObject, originalScale * 1.1f, 0.1f).setEase(LeanTweenType.easeOutQuad);
-
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         LeanTween.scale(gameObject, originalScale, 0.1f).setEase(LeanTweenType.easeOutQuad);
+    }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       
+        if (!hasInitialAnimation || animationCompleted)
+        {
+            ExecuteClickAnimation();
+        }
+    }
+
+    private void ExecuteClickAnimation()
+    {
+       
+        LeanTween.move(gameObject, finalPosition, animationDuration).setEase(animationButton);
     }
 }
