437aa10c566b849ba8709819f60dae482218a485
David Alejandro <99730787+DavidAlejandroHA@users.noreply.github.com>
2025-02-02 20:08:33 +0000

    Edit de colores de selección de cada botón

--- Assets/Scripts/Hotbar/Slot.cs
+++ Assets/Scripts/Hotbar/Slot.cs
@@ -6,15 +6,26 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class Slot : MonoBehaviour
 {
-    private Button currentButton;
+    public Color colorOnSelectedButton;
+    private Color _normalButtonColor;
+    private Button _currentButton;
+    private Image _buttonImage;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _currentButton = GetComponent<Button>();
+        _normalButtonColor = _currentButton.colors.normalColor;
+        _buttonImage = GetComponent<Image>();
+    }
     void Start()
     {
-        currentButton = GetComponent<Button>();
+        
     }
 
     public void ToggleHighlight()
     {
-
+        Debug.Log(gameObject);
+        GetComponent<Image>().color = _currentButton.colors.selectedColor;
     }
 }
