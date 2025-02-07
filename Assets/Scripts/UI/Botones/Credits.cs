b45d292390400aece26631860e77a5d0b3136c42
CaRoxas <carlotacarballolol@gmail.com>
2025-02-07 17:07:59 +0000

    Cambair localization y credit panel
    Cambiar cosas de la tabla de localizaciones para reajustes.
    Añadir una nueva variable al script de los créditos ya que no se volvían a ver en ningún momento.
    Añadir fuente a la cinemática
    Y ACTUALIZAR los botones con el menú de pausa

--- Assets/Scripts/UI/Botones/Credits.cs
+++ Assets/Scripts/UI/Botones/Credits.cs
@@ -7,32 +7,28 @@ using TMPro;
 public class Credits : MonoBehaviour
 {
     Vector3 startPos;
-    int finalPos = 814;
-    [SerializeField] int speed = 700;
+    Vector3 globalStartPos;
+    int finalPosY = 100;
+    int posRect = 1000;
+    [SerializeField] int speed = 12;
     // Start is called before the first frame update
+
     void Start()
     {
-        startPos = transform.position;
-        //Lucía María Alemán Marrero
-        //Carlota Carballo Falcón
-        //Omar Chávez González
-        //Sergio Delgado Salas
-        //Iván Domínguez Díaz
-        //Ángel Jesús Garnica Paz
-        //David Alejandro Hernández Alonso
-        //Idayra Inmaculada Pérez Delgado
-        //Conservatorio S/C de Tenerife
-        //Concept Artist + Game Designer + Main programmer + GUI Designer + Sound Designer + Sound Engineer + Particle Designer + GUI Integrators
-        //+ Modeling artists + Programmers + Animator 
+        startPos = transform.localPosition;
+        globalStartPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * Time.deltaTime * speed);
-        if (transform.position.y > finalPos)
+        if (transform.position.y > finalPosY)
         {
-            transform.position = startPos;
+            //transform.position = startPos;
+            float padreX = transform.position.x;
+            float nuevaPosX = padreX + startPos.x;
+            transform.position = new Vector3(nuevaPosX,startPos.y + posRect, globalStartPos.z);
         }
     }
 }
