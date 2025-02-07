dff4c1cd0431ada313bf4521d22cbf4160042ca4
David Alejandro <99730787+DavidAlejandroHA@users.noreply.github.com>
2025-02-07 23:39:43 +0000

    Bugfix colisiones jugador con enemigos al morir
    Los enemigos ya no chocan con el navMeshObstacle del jugador al morir, ya que este cuando se muere se desactiva y se reactiva al revivir. El layer del jugador ahora no choca con lo de los enemigos, pero en su lugar el jugador al usar el navMeshObstacle  simula esas colisiones de empuje

--- Assets/Scripts/Personaje_Main/Player.cs
+++ Assets/Scripts/Personaje_Main/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using Cinemachine;
 using UnityEngine.EventSystems;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour, IDamageable
 {
@@ -32,28 +33,51 @@ public class Player : MonoBehaviour, IDamageable
     //public ParticleSystem shootProyectileParticles;
     public GameObject particlesParent;
 
-    private ProyectilFabric proyectilFabric;
-    private bool canShoot = false;
-    private bool isShowingCrosier = false;
-    private bool isHoveringOverAButton = false;
+    private ProyectilFabric _proyectilFabric;
+    private bool _canShoot = false;
+    private bool _isShowingCrosier = false;
+    private bool _isHoveringOverAButton = false;
 
     private float _currentHealth;
     private float _maxHealth;
 
     public Animator animator;
 
-    private TimerMuerte timer;
+    private TimerMuerte _timer;
+    private CharacterController _characterController;
+    private NavMeshObstacle _navMeshObstacle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //_healthBar = GetComponentInChildren<HealthBar>();
+        _currentHealth = health;
+        _maxHealth = health;
+        _proyectilFabric = GetComponent<ProyectilFabric>();
+        _timer = gameObject.GetComponent<TimerMuerte>();
+        _characterController = gameObject.GetComponent<CharacterController>();
+        _navMeshObstacle = gameObject.GetComponent<NavMeshObstacle>();
+        _timer.Player = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageCooldown();
+        animator.SetBool("ataque", _isShowingCrosier);
+    }
 
     public void Die()
     {
         //throw new System.NotImplementedException();
-        timer.ActivateRespawnTimer(); // Desaparecer temporalmente + mostrar UI espera de respawn
+        _timer.ActivateRespawnTimer(); // Desaparecer temporalmente + mostrar UI espera de respawn
         ParticleSystem particulas = InstantiateParticlesOnPlayer(deathParticles);
         particulas.transform.position += Vector3.up * 0.1f; // Subir partículas ligeramente del suelo
         PlaceManager.Instance.DiscardCurrentTower(); // Cancelar la colocación de la torre actual, si es que
                                                      // se estaba colocando algo
         hotbar.DeselectCurrentButton(); // Deseleccionar botón actual
         hotbar.DisableHotbar(); // Deshabilitar hotbar
+        if (_navMeshObstacle) _navMeshObstacle.enabled = false; // Desactivar el hueco del jugador para que los 
     }
 
     public float GetHealth() { return _currentHealth; }
@@ -85,6 +109,7 @@ public class Player : MonoBehaviour, IDamageable
         // Calcular el centro del jugador y cambiar la posición de las partículas a ese centro
         particulas.transform.position = PlaceManager.Instance.GetGameObjectCenter(gameObject);
         hotbar.EnableHotbar(); // Habilitar hotbar
+        if (_navMeshObstacle) _navMeshObstacle.enabled = true; // Reactivar el hueco del jugador para simular un espacio
     }
     private ParticleSystem InstantiateParticlesOnPlayer(ParticleSystem pSys)
     {
@@ -98,11 +123,11 @@ public class Player : MonoBehaviour, IDamageable
     {
         if (Time.deltaTime != 0 && _currentHealth > 0) // Si el juego no está pausado y el jugador está vivo
         {
-            if (!UIUtils.IsPointerOverInteractableUIElement() && isShowingCrosier && canShoot && !isHoveringOverAButton
+            if (!UIUtils.IsPointerOverInteractableUIElement() && _isShowingCrosier && _canShoot && !_isHoveringOverAButton
             && ctx.started && !PlaceManager.Instance.bloqueoDisparo)
             { // Si tiene el bastón alzado y no hay cooldown y no se está haciendo click encima de un botón, se dispara
-                proyectilFabric.LanzarProyectil();
-                canShoot = false;
+                _proyectilFabric.LanzarProyectil();
+                _canShoot = false;
                 GameObject particles = ShootingMagicParticlesPool.Instance.GetMagicShootingParticle();
                 particles.transform.position = shootingSource.transform.position;
                 particles.GetComponent<ParticleSystem>().Play();
@@ -112,48 +137,30 @@ public class Player : MonoBehaviour, IDamageable
 
     public void ShowCrosier()
     {
-        isShowingCrosier = true;
+        _isShowingCrosier = true;
         _currentCooldown = cooldown;
     }
 
     public void HideCrosier()
     {
-        isShowingCrosier = false;
+        _isShowingCrosier = false;
     }
     public bool CheckIfIsShowingCrosier()
     {
-        return isShowingCrosier;
+        return _isShowingCrosier;
     }
 
     private void ManageCooldown()
     {
-        if (!canShoot)
+        if (!_canShoot)
         {
             _currentCooldown -= Time.deltaTime;
 
             if (_currentCooldown <= 0)
             {
-                canShoot = true;
+                _canShoot = true;
                 _currentCooldown = cooldown;
             }
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //_healthBar = GetComponentInChildren<HealthBar>();
-        _currentHealth = health;
-        _maxHealth = health;
-        proyectilFabric = GetComponent<ProyectilFabric>();
-        timer = gameObject.GetComponent<TimerMuerte>();
-        timer.Player = this;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        ManageCooldown();
-        animator.SetBool("ataque", isShowingCrosier);
     }
 }
